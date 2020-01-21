8d15258b34b1dad684cacaf17e2e7b54c1bafa0e
chooeeh <chchgml10@gmail.com>
2020-01-21 16:02:12 +0900

    2020/01/21 프로그래머스

--- Level1/K번째수.cpp
+++ Level1/K번째수.cpp
@@ -1,11 +1,11 @@
-//뭔가 채점이 잘못되었다! 채점을 하면 이상하다.. 
 #include <vector>
 #include<algorithm>
 using namespace std;
 
-vector<int> solution(vector<int> array, vector<vector<int>> commands) {
+vector<int> solution(vector<int> input, vector<vector<int>> commands) {
     vector<int> result;
     	for(int i = 0 ; i < commands.size() ; i++){
+            vector<int> array = input;
 		    if(commands[i][0] == commands[i][1]){
 			    result.push_back(array[commands[i][0] - 1]);
 		    }else{
